b4661577cef57a0f14f86f219357feb04dd8d3bd
dhreyes <2590@holbertonschool.com>
2021-02-03 18:10:43 +0000

    Answering and completing task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,13 @@
-Placeholder text
+#include <stdio.h>
+/* Above is needed to access a pre-existing library */
+
+/* First part of a code always needed */
+
+int main(void)
+{
+
+/* This prints out the string in parenthesis */
+
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
