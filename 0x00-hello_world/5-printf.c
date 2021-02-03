78feb2c61cd53c921746d47b4026e0a145266e86
dhreyes <2590@holbertonschool.com>
2021-02-03 18:36:41 +0000

    Answer for task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - implementing printf use
+ * Return: 0 if all goes well
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
