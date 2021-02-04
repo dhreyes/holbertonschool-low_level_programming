ba437354233ce95231a1cb9352aeca71d600d195
dhreyes <2590@holbertonschool.com>
2021-02-04 20:40:26 +0000

    Answer for task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - printing base 10 digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
