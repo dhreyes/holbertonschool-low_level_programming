8021ee956b7b7d7533571c4f4ff25575b026b90c
dhreyes <2590@holbertonschool.com>
2021-02-04 16:16:56 +0000

    Making betty happy

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-/* main-entry point */
-/* betty style doc for function main goes there */
+/**
+ * main - determines the sign of n
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
