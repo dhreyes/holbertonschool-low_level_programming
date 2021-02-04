c688df57ec13fe3d49c2d3b4d2cc0b4d95ab86b0
dhreyes <2590@holbertonschool.com>
2021-02-04 23:32:46 +0000

    Correction of code to task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,14 +5,15 @@
  */
 int main(void)
 {
-	char dg;
+	int dg;
 
-	for (dg = '0'; dg <= '9'; dg++)
-		putchar(dg);
-			if (dg <= '9')
-				putchar(',');
-				putchar(' ');
-	
+	for (dg = 48; dg <= 57; dg++)
+	{
+	putchar(dg);
+	if (dg < 57)
+	putchar(',');
+	putchar(' ');
+	}
 	putchar('\n');
 
 	return (0);
