eee710605d36094fdaa970cfa6a771f052a60b9d
dhreyes <2590@holbertonschool.com>
2021-02-05 00:55:25 +0000

    Answer to task 11

---
+++ 0x01-variables_if_else_while/test_for_10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - possible combinations of single digits
+ * Return: Always successful (0)
+ */
+int main(void)
+{
+	int d1, d2;
+
+	for (d1 = 0; d1 <= 9; d1++)
+	{
+		for (d2 = 0; d2 <= 9; d2++)
+		{
+			putchar(d1 + '0');
+			putchar(d2 + '0');
+			if ((d1 * 10 + d2) < 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
