c429c956008727d5dfcbd3f82e76e903cf9f961e
dhreyes <2590@holbertonschool.com>
2021-02-08 15:31:38 +0000

    Getting started with task 0

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * main - print Holberton
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x;
+
+	char h[9] = "Holberton";
+
+	for (x = 0; x < 10; x++)
+	{
+		_putchar(h[x]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
+
