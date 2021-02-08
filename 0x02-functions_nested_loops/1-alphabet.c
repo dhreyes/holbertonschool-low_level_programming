884e8c5fde04dd8ef462d7d6fb6581de1013c8c1
dhreyes <2590@holbertonschool.com>
2021-02-08 20:03:58 +0000

    Figuring out task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * main - print alphabet in lowercase
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{
+	char al;
+
+	for (al = 'a'; al <= 'z'; al++)
+	{
+		_putchar(al);
+	}
+	_putchar('\n');
+}
