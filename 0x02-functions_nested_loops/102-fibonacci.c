a1665493d66f16f340c86904b7639ec36eeba210
dhreyes <2590@holbertonschool.com>
2021-02-09 19:52:17 +0000

    Answer task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *main - prints the first 50 fibonacci numbers starting with 1 and 2
+ *
+ *Return: always 0
+ */
+int main(void)
+{
+	int count;
+	long int f1 = 1;
+	long int f2 = 2;
+	long int r;
+	int flimit = 50;
+
+	printf("1, 2, ");
+	for (count = 3; count <= flimit; count++)
+	{
+		next = f1 + f2;
+		f1 = f2;
+		f2 = r;
+		printf("%ld", next);
+		if (count < flimit)
+			printf(", ");
+	}
+	printf("\n");
+
+	return (0);
+}
