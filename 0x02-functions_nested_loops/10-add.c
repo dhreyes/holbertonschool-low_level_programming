0a4384db796ccb3686aa5e02abf9b1856ce8fb7c
dhreyes <2590@holbertonschool.com>
2021-02-10 01:40:32 +0000

    Syntax fixes

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,11 @@
 /**
  * add - adds two integers
+ * @n1: input of 1st integer
+ * @n2: input of 2nd integer
  * Return: added integers
  */
 
-int add(int n1, n2)
+int add(int n1, int n2)
 {
-	return(n1 + n2);
+	return (n1 + n2);
 }
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,17 +1,16 @@
 #include "holberton.h"
+#include <stdio.h>
+
 /**
  * main - check the code for Holberton School students.
- * Return: Always 0 (success)
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	print_times_table(3);
-	_putchar('\n');
-	print_times_table(5);
-	_putchar('\n');
-	print_times_table(98);
-	_putchar('\n');
-	print_times_table(12);
-	_putchar('\n');
-	return (0);
+    int n;
+
+    n = add(89, 9);
+    printf("%d\n", n);
+    return (0);
 }
