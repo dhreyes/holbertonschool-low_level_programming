92957a3e4fd1b50c9d6a65243846376e54d48b2b
dhreyes <2590@holbertonschool.com>
2021-02-11 19:22:36 +0000

    Answer task 8

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -2,14 +2,12 @@
 
 /**
  * main - check the code for Holberton School students.
- *
  * Return: Always 0.
  */
 int main(void)
 {
-    print_diagonal(0);
-    print_diagonal(2);
-    print_diagonal(10);
-    print_diagonal(-4);
+    print_square(2);
+    print_square(10);
+    print_square(0);
     return (0);
 }
