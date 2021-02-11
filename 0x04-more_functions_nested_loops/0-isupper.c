b39a55d121ff3bf1141b87725a25a67a176496a0
dhreyes <2590@holbertonschool.com>
2021-02-11 16:32:50 +0000

    Answer task 0 and add headerfile

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *_isupper - checks to see if uppercase
+ *@c - character input
+ * Return: 1 if upper, else 0
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This README will contain all of the answers to mandatory and advanced tasks for project 0x04-more_functions_nested_loops
