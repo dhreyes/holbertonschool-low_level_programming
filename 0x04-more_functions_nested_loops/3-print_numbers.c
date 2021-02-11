14525e3d0196f2afbd20f029edab9c490fb73cd4
dhreyes <2590@holbertonschool.com>
2021-02-11 17:33:26 +0000

    Answer task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *print_numbers - prints digits 0 to 9
+ *Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		_putchar(i + '0');
+	_putchar('\n');
+}
