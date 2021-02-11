660d52cab4b9ed673c4c5722ccdd43bd459bfc49
dhreyes <2590@holbertonschool.com>
2021-02-11 17:36:21 +0000

    Answer task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *print_most_numbers - prints digits 0 to 9, except 2 and 4
+ *Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+	_putchar('\n');
+}
