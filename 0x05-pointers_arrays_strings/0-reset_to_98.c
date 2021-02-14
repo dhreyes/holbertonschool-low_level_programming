ec7420e991f8692dd6f4aa9786b856164bce4bcc
dhreyes <2590@holbertonschool.com>
2021-02-14 01:56:53 +0000

    Readme, task 0, and header file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *reset_to_98 - updates the parameter a pointer indicates to 98
+ *@n: pointer to update
+ *Return: none
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This README will contain all answers for mandatory and advanced tasks for project 0x05-pointers_arrays_strings
