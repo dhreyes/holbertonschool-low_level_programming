848210b172e959339b6bb1f4d5f0e696e0f02aaa
dhreyes <2590@holbertonschool.com>
2021-02-15 17:52:05 +0000

    Answer task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+/**
+ *rev_string - reverses string
+ *@s: input
+ *Return: reversed string
+ */
+
+int string_length(char *pointer);
+void rev_string(char *s)
+{
+	int l, c;
+	char *begin, *end, temp;
+
+	l = string_length(s);
+	begin = s;
+	end = s;
+
+	for (c = 0; c < l - 1; c++)
+		end++;
+
+	for (c = 0; c < (l / 2); c++)
+	{
+		temp = *end;
+		*end = *begin;
+		*begin = temp;
+
+		begin++;
+		end--;
+	}
+}
+
+/**
+ *string_length - finds length of a string
+int string_length(char *pointer)
+ *@pointer: pointer
+ *Return: c
+ */
+
+int string_length(char *pointer)
+{
+	int c = 0;
+
+	while (*(pointer + c) != '\0')
+		c++;
+
+	return (c);
+}
