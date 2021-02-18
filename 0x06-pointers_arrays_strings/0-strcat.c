4108438214056b3836554253e78e629308bc33bf
dhreyes <2590@holbertonschool.com>
2021-02-18 18:11:36 +0000

    add readme, initial answers for task 0, 1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+/**
+ * *_strcat - concatenates 
+ * @dest: first string waiting for attachment
+ * @src: string to be attached
+ * Return: Always 0 (Success)
+ */
+char *_strcat(char *dest, char *src)
+{
+	char* ptr = dest + _strlen(dest);
+
+	while (*src != '\0')
+		*ptr++ = *src++;
+	*ptr = '\0';
+	return (dest);
+}
+/**
+ * _strlen - calc length of string
+ * @str: input of string
+ * Return: index
+ */
+int _strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This project will contain all of the answers for mandatory and advanced tasks for project 0x06-pointers_arrays_strings
