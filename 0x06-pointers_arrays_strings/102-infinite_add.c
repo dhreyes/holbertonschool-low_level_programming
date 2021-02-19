829eb8e8631e46556505377113a8f73a30c6ff55
dhreyes <2590@holbertonschool.com>
2021-02-19 03:33:04 +0000

    Restructured completely

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -1,13 +1,11 @@
-#include "holberton.h"
-#include <stdio.h>
 /**
- * *infinite_add - Short description, single line
- * @n1: 1st
- * @n2: 2nd
- * @r: placeholder
- * @size_r: size of r
- * Description: Longer description of the function)?
- * Return: Description of the returned value
+ * infinite_add - Add up two numbers stored in given char arrays
+ * @n1: The first number
+ * @n2: The second number
+ * @r: Pointer to the buffer to store result
+ * @size_r: The size of the buffer
+ *
+ * Return: 0 if buffer too small to store result, else return pointer to buffer
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
@@ -20,13 +18,20 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		j++;
 	while (size_r != 0 && i != 0 && j != 0)
 	{
-		one = (n1[i] - '0');
-		two = (n1[i] - '0');
+		if (n1[0] == '-')
+			one = (n1[i] - '0');
+		else
+			one = (n1[i] - '0') * -1;
+		if (n2[0] == '-')
+			two = (n1[i] - '0');
+		else
+			two = (n1[i] - '0') * -1;
+
 		temp = one + two + remainder;
 		if (temp > 9)
 		{
 			remainder = temp / 10;
-			temp = temp % 10;
+			temp %= 10;
 		}
 		r[size_r--] = temp;
 		printf("%d\n", r[size_r + 1]);
