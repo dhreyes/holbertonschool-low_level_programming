7002229841540654091d9244f8c1b2286118607c
dhreyes <2590@holbertonschool.com>
2021-02-22 16:35:24 +0000

    Optimizing code for checker

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,19 +8,11 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	if (s == '\0')
-	{
-		return ('\0');
-	}
 	while (s[i] != '\0')
 	{
 		if (s[i] == c)
 			return (&s[i]);
 		i++;
-	}
-	if (s[i] == '\0')
-	{
-		return (&s[i]);
 	}
 	return ('\0');
 }
