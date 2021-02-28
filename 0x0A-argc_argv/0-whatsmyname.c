76a60ee77d27d341cca1cd3b47778ba5cdd1604c
dhreyes <2590@holbertonschool.com>
2021-02-28 18:31:02 +0000

    Betty fixes

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,9 +2,10 @@
 /**
  *main - prints name of function (self)
  *@argc: count of argv
- *@*argv: array of strings
+ *@argv: array of strings
+ *Return: Always 0 (success)
  */
-int main (__attribute__((unused)) int argc, char *argv[])
+int main(__attribute__((unused))int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
