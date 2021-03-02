a7f83658e9d71e9b894d04d419913c1dae61a6cb
dhreyes <2590@holbertonschool.com>
2021-03-02 18:12:05 +0000

    Initial task 0 and readme

---
+++ 0x0B-malloc/0-create_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars of specified size
+ * initialized to a char
+ * @size: size of new array
+ * @c: char with which to initialize all spaces in the array
+ *
+ * Return: a pointer to the array, or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *m;
+
+	m = malloc(sizeof(char) * size);
+	if (m == NULL)
+		return (NULL);
+	while (size > 0)
+		m[--size] = c;
+
+	return (m);
+}
---
+++ 0x0B-malloc/README.md
@@ -0,0 +1,1 @@
+This project will contain the mandatory and advanced tasks for project 0x0B-malloc
