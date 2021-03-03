d7bd06f10012cfd19b4f1129986937df971d600c
dhreyes <2590@holbertonschool.com>
2021-03-03 02:17:18 +0000

    Answer task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+/**
+ * free_grid - frees  the grid
+ * @grid: grid to clear
+ * @height: come on rlly u know
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
