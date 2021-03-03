19f171e249980bd94c5673e090a2f4d2b57695bd
dhreyes <2590@holbertonschool.com>
2021-03-03 00:17:06 +0000

    Answer task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,3 +3,44 @@
  * alloc_grid - create 2 dimensional array
  * @width: # of columns
  * @height: # of rows
+ * Return: Pointer to 2D array, else null
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	i = 0;
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	grid = malloc(height * sizeof(*grid));
+	if (grid == NULL)
+	{
+		free(grid);
+		return (NULL);
+	}
+	while (i < height)
+	{
+		grid[i] = malloc(width * sizeof(**grid));
+		if (grid[i] == NULL)
+		{
+			i--;
+			while (i >= 0)
+			{
+				free(grid[i]);
+				i--;
+			}
+			free(grid);
+			return (NULL);
+		}
+		j = 0;
+		while (j < width)
+		{
+			grid[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
+	i = 0;
+	return (grid);
+}
