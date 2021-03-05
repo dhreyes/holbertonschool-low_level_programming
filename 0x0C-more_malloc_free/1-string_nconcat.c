eeec0d27a78b6088f32eea6208abfd804f5ca1d7
dhreyes <2590@holbertonschool.com>
2021-03-05 16:04:26 +0000

    FINAL FIX, incorporate if statement into while loops

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,39 +11,34 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s3;
-	unsigned int i, len1, len2, j;
+	unsigned int i, len1, j, count;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	i = len1 = len2 = 0;
+	i = len1 = 0;
 	while (s1[len1] != '\0')
 	{
 		len1++;
 	}
-	while (s2[len2] != '\0')
-	{
-		len2++;
-	}
-	if (n >= len2)
-		n = len2;
-	
-	s3 = malloc((len1 + n + 1) * sizeof(char));
+	count = (len1 + n) * sizeof(char);
+	s3 = malloc(count + 1);
 	if (s3 == NULL)
 		return (NULL);
 	i = 0;
-	while (i < len1)
+	while (i < count && s1[i] != '\0')
 	{
 		s3[i] = s1[i];
 		i++;
 	}
 	j = 0;
-	while (j <= n)
+	while (i < count && s2[j] != '\0')
 	{
 		s3[i] = s2[j];
 		i++;
 		j++;
 	}
+	s3[i] = '\0';
 	return (s3);
 }
