a382ff793ff5e0465985e6c37cd7995a3fb03235
dhreyes <2590@holbertonschool.com>
2021-03-05 00:04:53 +0000

    Answer task 4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _realloc - free up old space allocate new space
+ *@ptr: ptr to return with new size
+ *@old_size: name says it all
+ *@new_size: ditto
+ *Return: ptr to new size
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (old_size == new_size)
+		return (ptr);
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	free(ptr);
+	ptr = malloc(new_size);
+	return (ptr);
+}
