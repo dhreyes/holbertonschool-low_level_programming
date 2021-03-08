5c0f70e9b3ac0465aa2851527e182ecef8b3ab25
dhreyes <2590@holbertonschool.com>
2021-03-08 16:05:16 +0000

    Initial commit of readme, holberton.h, and task 0

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef 0-object_like_macro.h
+#define 0-object_like_macro.h
+
+#define SIZE (x) 1024
+
+#endif // #ifndef 0-object_like_macro.h
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+This will contain the answers to mandatory and advanced tasks for project 0x0D-preprocessor
