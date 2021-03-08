d8bbe74aadb71c13c4564f32fccc4ab6879d0ae8
dhreyes <2590@holbertonschool.com>
2021-03-08 22:48:41 +0000

    Brace fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,25 +8,17 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-	{
 		return;
-	}
 	if (d->age < 0)
-	{
 		printf("Age: (nil)");
-	}
 	else
 		printf("%f\n", d->age);
 	if (d->name == NULL)
-	{
 		printf("Name: (nil)");
-	}
 	else
 		printf("%s\n", d->name);
 	if (d->owner == NULL)
-	{
 		printf("Owner= (nil)");
-	}
 	else
 		printf("%s\n", d->owner);
 }
