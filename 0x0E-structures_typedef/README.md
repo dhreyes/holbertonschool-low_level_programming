aaa1fef7697d685b7dd848fac0fb3cbf8f1cfd1c
dhreyes <2590@holbertonschool.com>
2021-03-08 21:00:56 +0000

    Initial readme, and task 0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This project will contain mandatory and advanced task answers for project 0x0E-structures_typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,10 @@
+#ifndef DOGE
+#define DOGE
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
