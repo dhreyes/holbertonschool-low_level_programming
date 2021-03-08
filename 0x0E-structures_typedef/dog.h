ec7de1820b0e1e8b0b3477de8c5f3bc1d89a960a
dhreyes <2590@holbertonschool.com>
2021-03-08 23:34:12 +0000

    Include typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,17 +6,12 @@
  * @age: age of dog
  * @owner: owner of dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-
-struct dog_t {
-	struct dog *dog;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
