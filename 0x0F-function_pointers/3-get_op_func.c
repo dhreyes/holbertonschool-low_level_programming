fa70f5c8912b91a9f1a7c338d08ffba6f21efaf2
dhreyes <2590@holbertonschool.com>
2021-03-10 22:55:47 +0000

    3-get op while loop change

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,9 +17,10 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i]).op == *s)
+	while (ops[i].op != NULL)
 	{
-		return ((ops[i]).f);
+		if (*(ops[i]).op == *s)
+			return ((ops[i]).f);
 		i++;
 	}
 	return (NULL);
