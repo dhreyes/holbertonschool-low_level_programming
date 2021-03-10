9918c302e44b74373739859a229029724ca9355f
dhreyes <2590@holbertonschool.com>
2021-03-10 22:48:55 +0000

    First attempt

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main - calculates 2 arguments
+ * @argc: num of arguments
+ * @argv: arguments passed to function
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	char *p;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	p = argv[2];
+
+	printf("%d\n", get_op_func(p)(num1, num2));
+	return (0);
+}
