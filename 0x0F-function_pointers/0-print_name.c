15491b7cba9a6b476f0477e4bea22fb85a616234
dhreyes <2590@holbertonschool.com>
2021-03-10 17:43:50 +0000

    Initial commit, readme, .h file, task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ *
+ *
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL)
+		f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+This project will contain mandatory and advanced tasks for project 0x0F-function_pointers
