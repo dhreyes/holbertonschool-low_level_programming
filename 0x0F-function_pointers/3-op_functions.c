b3d962e74bfd021b3c8666d501658304854ecd51
dhreyes <2590@holbertonschool.com>
2021-03-10 20:47:09 +0000

    wrote basic functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * op_add - adds a and b
+ * @a: int a
+ * @b: int b
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtracts b from a
+ * @a: int a
+ * @b: int b
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies a and b
+ * @a: int a
+ * @b: int b
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides b into a
+ * @a: int a
+ * @b: int b
+ * Return: quotient of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - gets remainder of a and b
+ * @a: int a
+ * @b: int b
+ * Return: remainder of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
