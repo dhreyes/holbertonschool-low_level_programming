18bdf27abec3bc1984bcb27a1c438ae1e5fd8d78
dhreyes <2590@holbertonschool.com>
2021-03-10 23:13:30 +0000

    Updated for strings longer than intended

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 	num2 = atoi(argv[3]);
 	p = argv[2];
 
-	if (get_op_func(p) == NULL)
+	if (get_op_func(p) == NULL || p[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
