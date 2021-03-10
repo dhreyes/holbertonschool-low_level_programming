fc08d500f1e7bba9d7feaa18c83c1de7499b869d
dhreyes <2590@holbertonschool.com>
2021-03-10 17:51:44 +0000

    Betty fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include "function_pointers.h"
 /**
- *
- *
- *
+ * print_name - uses function pointers to print name
+ * @name: name input
+ * @f: function pointer
+ * Return: returns ptr to function
  */
 void print_name(char *name, void (*f)(char *))
 {
