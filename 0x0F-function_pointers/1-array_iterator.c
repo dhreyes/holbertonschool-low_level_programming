71cb76ea645595620ca96e322b8002144201aafe
dhreyes <2590@holbertonschool.com>
2021-03-10 18:30:15 +0000

    Answer task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - uses function ptr to iterate array
+ * @array: array to be iterated
+ * @size: size of array
+ * @action: ptr to function
+ * Return: elements of array
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int idx;
+
+	idx = 0;
+
+	if (array != NULL || action != NULL)
+	{
+		while (idx < size)
+		{
+			action(array[idx]);
+			idx++;
+		}
+	}
+}
