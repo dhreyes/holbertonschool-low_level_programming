04fdaca1221c7b407d921ec70106e717a135cda7
dhreyes <2590@holbertonschool.com>
2021-03-12 03:15:05 +0000

    Revert to only check 14 not working

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,24 +11,21 @@ void print_all(const char * const format, ...)
 {
 	unsigned int idx = 0;
 	va_list op;
-	char *s, *sep1 = "", *sep2 = ", ";
+	char *s;
 
 	va_start(op, format);
-	while (format[idx] && (format))
+	while (format[idx] != '\0' && (format))
 	{
 		switch (format[idx])
 		{
 			case 'c':
-				printf("%s", sep1), printf("%c", va_arg(op, int));
-				sep1 = sep2;
+				printf("%c", (char) va_arg(op, int));
 				break;
 			case 'i':
-				printf("%s", sep1), printf("%d", va_arg(op, int));
-				sep1 = sep2;
+				printf("%d", va_arg(op, int));
 				break;
 			case 'f':
-				printf("%s", sep1), printf("%f", va_arg(op, double));
-				sep1 = sep2;
+				printf("%f", (float) va_arg(op, double));
 				break;
 			case 's':
 				s = va_arg(op, char *);
@@ -37,10 +34,14 @@ void print_all(const char * const format, ...)
 					printf("(nil)");
 					break;
 				}
-				printf("%s", sep1), printf("%s", s);
-				sep1 = sep2;
+				printf("%s", s);
 				break;
+			default:
+				idx++;
+				continue;
 		}
+		if (format[idx + 1] != '\0')
+			printf(", ");
 		idx++;
 	}
 	printf("\n");
