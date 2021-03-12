c6377ef67fd6357f0e31baee1dce402b7634abae
dhreyes <2590@holbertonschool.com>
2021-03-12 17:15:31 +0000

    Len fix

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,12 +1,12 @@
 section .data
-	msg db "Hello, Holberton ",10
+	msg db "Hello, Holberton",10
 section .text
 	global main
 main:
 	mov rax,1
 	mov rdi,1
 	mov rsi,msg
-	mov rdx,18
+	mov rdx,17
 	syscall
 
 	mov rax,60
