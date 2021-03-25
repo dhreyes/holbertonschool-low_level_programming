5863425322fc7a16ff125c1ceb5d2042abb84249
dhreyes <2590@holbertonschool.com>
2021-03-25 23:05:41 +0000

    Betty fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- *
- *
- *
+ * print_binary - prints binary of n
+ * @n: decimal to be converted
+ * Return: none
  */
 void print_binary(unsigned long n)
 {
