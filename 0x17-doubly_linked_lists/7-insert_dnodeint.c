89fff06fe1ce2a9686a1d8e6a107696fd0ff613d
dhreyes <2590@holbertonschool.com>
2021-05-03 03:51:15 +0000

    Start task 7

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - insert new node at index with n value
+ * @h: start of dlistint_t
+ * @idx: index to insert new node
+ * @n: int value of new node
+ * Return: address of new node, else NULL
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node;
+
+
+}
