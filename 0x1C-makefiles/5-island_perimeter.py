e8cfe3e52517ddc39ebf58f11fb18f45140e12d4
dhreyes <2590@holbertonschool.com>
2021-07-09 19:23:51 -0400

    Fix return statement

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,4 +1,9 @@
 #!/usr/bin env python3
+"""
+Determines peremiter of island
+"""
+
+
 def island_perimeter(grid):
     """
     :type grid: List[List[int]]
@@ -15,4 +20,4 @@ def island_perimeter(grid):
                     edge += 1
                     if j > 0 and grid[i][j-1] == 1:
                         edge += 1
-                        return land * 4 - edge * 2
+    return land * 4 - edge * 2
